c3dde90395e36fab1978cad8d9e316141fa026ba
Terry Allee <terryallee@westernwyoming.edu>
2025-01-26 18:08:19 +0000

    Ready for grading.

--- fix-the-errors-2/main.cpp
+++ fix-the-errors-2/main.cpp
@@ -1,6 +1,6 @@
 //
-// Name
-// Date
+// Name: Terry Allee
+// Date: January 26
 // Fix the Errors #2 Programming Project
 // COSC 1030
 //
@@ -16,7 +16,7 @@ int main()
 
 	float number;
 	int divider;
-	divider = 0;
+	divider = 2; // Set divider by 2 to avoid dividing by 0
 
 	cout << "Hi there" << endl;
 	cout << "Please input a number and then hit return" << endl;
