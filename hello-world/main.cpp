f33e30149de6ec8c75452dd69effb864279afbbb
Terry Allee <terryallee@westernwyoming.edu>
2025-01-26 18:38:59 +0000

    ready to grade

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,14 @@
 //
-// Name
-// Date
+// Name: Terry Allee
+// Date: January 26, 2025
 // Hello World Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
