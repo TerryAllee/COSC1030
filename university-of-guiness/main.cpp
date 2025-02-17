ec17422cef6fa8672733628217079295e17a256f
Terry Allee <terryallee@westernwyoming.edu>
2025-02-17 03:17:36 +0000

    Ready for grading

--- university-of-guiness/main.cpp
+++ university-of-guiness/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -17,11 +18,41 @@ int main() {
 
     //Enter residency status
     cout << "Enter the residency status (in-state or out-of-state): ";
-    cin >> residency;
+    getline(cin, residency);
 
     // Convert to lowercase for consistency
     for (char &c : residency) c = tolower (c);
+    cout << "Residency entered: " << residency << endl;
 
     // Determine tuition and room & board costs
-    if 
+    if (residency == "in-state" || residency == "instate") {
+        tuition = 3000;
+        roomAndBoard = 2500;
+    } else if (residency == "out-of-state" || residency == "outstate" || residency == "outofstate") {
+        tuition = 4500;
+        roomAndBoard = 3500;
+    } else {
+        cout<< "Invalid residency status. Please enter 'in-state' or 'out-of-state'." <<endl;
+        return 1;
+    }
+
+    // Enter room and board
+    cout << "Do your require room and board? (Y/N): ";
+    cin >> roomBoard;
+    roomBoard = tolower(roomBoard);
+    
+    //Validate and calulate the total cost
+    if (roomBoard == 'y') {
+        totalBill = tuition + roomAndBoard;
+    } else if (roomBoard == 'n') {
+        totalBill = tuition;
+    } else{
+        cout << "Invalid input for room and board. Please enter 'Y' or 'N'. " << endl;
+        return 1;
+      }
+
+      //Output total bill
+      cout << "Your total cost for the semester is $" <<totalBill << endl;
+
+      return 0;
 }
