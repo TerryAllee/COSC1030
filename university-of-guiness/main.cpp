b08f125274955b442d9a5c8eedf292be03babd46
Terry Allee <terryallee@westernwyoming.edu>
2025-02-17 03:17:18 +0000

    Ready for grading

--- university-of-guiness/main.cpp
+++ university-of-guiness/main.cpp
@@ -1,7 +1,27 @@
 //
-// Name
-// Date
+// Name: Terry Allee
+// Date: February 15th
 // University of Guiness Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string residency;
+    char roomBoard;
+    double tuition, roomAndBoard, totalBill;
+
+    //Enter residency status
+    cout << "Enter the residency status (in-state or out-of-state): ";
+    cin >> residency;
+
+    // Convert to lowercase for consistency
+    for (char &c : residency) c = tolower (c);
+
+    // Determine tuition and room & board costs
+    if 
+}
