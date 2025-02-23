23fc4345c601c12a681710f15418357e9133a2e5
Terry Allee <terryallee@westernwyoming.edu>
2025-02-23 17:44:34 +0000

    Ready for grading! Thank you

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,30 @@
 //
-// Name
-// Date
+// Name: Terry Allee    
+// Date: Feb 23, 2025
 // Calories Burned Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    // Constant for calories burned per minute
+    const double CALS_BURNED_PER_MINUTE = 3.0;
+
+    // Number of minutes
+    int minutes;
+
+    // Calories burned
+    double calories;
+
+    for (minutes = 10; minutes <= 30; minutes += 5)
+    {
+        calories = minutes * CALS_BURNED_PER_MINUTE;
+        cout << "After " << minutes << " minutes you burned "
+            << calories << "calories. \n";
+    }
+    return 0;
+
+}
--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -4,14 +4,30 @@
 // Restaurant Bill Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include < iomanip>
 
+<<<<<<< HEAD
+using namespace std;
+
+=======
 #include <iostream>
 
 using namespace std;
 
+>>>>>>> ec17422cef6fa8672733628217079295e17a256f
 // Variable declarations
 
 
+<<<<<<< HEAD
+int main() {}
+    // Constants for the tax rate and tip percentage
+    const double TAX_RATE = 0.0675; // 6.75% tax rate
+    const double TIP_RATE = .20; // 20% tip 
+    
+    // Get the amount of the purchase.
+    double mealCost = 88.67; //Cost of the meal
+=======
 int main() {
     // Constants for the tax rate and tip percentage
     const double TAX_RATE = 0.0675; // 6.75% tax rate
@@ -19,6 +35,7 @@ int main() {
     
     // Get the amount of the purchase.
     double mealCost = 88.67; //Cost of the meal
+>>>>>>> ec17422cef6fa8672733628217079295e17a256f
 
 
     // Calculate the sales tax.
@@ -30,6 +47,19 @@ int main() {
     // Calculate the total of the sale.
     double totalBill = mealCost + taxAmount + tipAmount;
 
+<<<<<<< HEAD
+    // Print information about the sale.
+    cout << fixed << setprecision(2);
+    cout << "Meal Cost: $" << mealCost << endl;
+    cout << "Tax Amount: $" << taxAmount << endl;
+    cout << "Tip Amount: $" << tipAmount << endl;
+    cout << "Total Bill: $" << totalBill << endl;
+
+
+    return 0 ;
+}
+
+=======
     // Print information about the sale
     cout << "Meal Cost: $" << mealCost << endl;
     cout << "Tax Amount: $" << taxAmount << endl;
@@ -40,3 +70,4 @@ int main() {
     return 0 ;
 
 }
+>>>>>>> ec17422cef6fa8672733628217079295e17a256f
