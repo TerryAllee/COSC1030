f45f087e1c2ac3002889a0ce747d24084c4e30fe
Terry Allee <terryallee@westernwyoming.edu>
2025-04-12 21:36:24 +0000

    Ready for grading

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,36 @@
 //
-// Name
-// Date
+// Name Terry Allee
+// Date: April 12, 2025
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+int doSomething(int *x, int *y);
+
+int main()
+{
+    int x = 5,  y = 2;
+
+    cout << "Before calling the function:\n"
+         << "x = " << x << endl;
+         cout << "y = " << y << endl << endl;
+    
+    cout << "The functions returns " << doSomething(&x, &y) << endl << endl;
+
+    cout << "After calling the function: \n"
+         << "x = " << x << endl;
+        cout << "y = " << y << endl;
+
+    return 0;
+
+}
+
+int doSomething(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y + 10;
+    *y = temp * 10;
+    return *x + *y;
+}
