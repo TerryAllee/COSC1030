0828c3b63165973ce08f32bcbcbb6c936d432d07
Terry Allee <terryallee@westernwyoming.edu>
2025-04-29 01:52:47 +0000

    Ready to grade

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,38 @@
 //
-// Name
-// Date
+// Name: Terry Allee
+// Date: April 28
 // String Length Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+// Function will count how many characters are in the string. 
+int countCharacters(const char *str) {
+    int count = 0;
+    //Loop until you find the end of the string
+    while (str[count] != '\0') {
+        count++;
+    }
+
+    // Return number of characters
+    return count;
+}
+
+int main () {
+    const int SIZE = 100;
+    char inputString[SIZE];
+
+    // Ask the user to enter a string
+    cout << "Please enter a string: ";
+    cin.getline (inputString, SIZE);
+
+    // Call the function to count characters and store the result
+    int length = countCharacters(inputString);
+
+    //Display the number of characters
+    cout << "Your string has " << length << " characters." << endl;
+
+    return 0;
+}
